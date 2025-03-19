fa7c0aa44caa6644d0f43a4391bb365ef8672229
vlzJuan <valencellojuan@gmail.com>
2025-03-18 22:17:39 -0300

    Agregado de rutina para actualizar la lista y agregar un amigo. Esqueleto de rutinas para sortear y mostrar resultado.

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim();//.toLowerCase(); 
+
+    if (nombre === "") {
+        alert("Error: El nombre agregado no puede ser vacio, reintentelo.");
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya está en la lista.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = amigos.map(amigo => `<li>${amigo}</li>`).join("");
+}
+
+function sortearAmigo() {
+    if (amigos.length < 2) {
+        alert("Deben haber al menos dos participantes para el sorteo.");
+        return;
+    }
+
+    mostrarResultado(amigos);
+}
+
+function mostrarResultado(listaSorteada) {
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = "Tu amigo secreto sorteado es: A IMPLEMENTAR."; // Aca implemento elegir un amigo especifico aleatorio.
+}
